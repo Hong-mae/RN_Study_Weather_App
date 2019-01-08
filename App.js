b0a25bdd1dc875a5558797944487ae0890f4ed6c
P-bin <qkrgksqls9@gmail.com>
2019-01-08 15:30:27 +0900

    Complete Project

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-    AppRegistry, FlatList, StyleSheet, Text, View, ActivityIndicator
+    AppRegistry, FlatList, StyleSheet, Text, View, ActivityIndicator,
+    StatusBar
 } from 'react-native';
 import * as Progress from 'react-native-progress';
+import Weather from './Weather';
 
 const API_KEY = "87c3f9d1c2e1e9cc770b9f479b4e2aea";
 
-export default class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state ={
-            isLoading: true,
-            progress: 0,
-            indeterminate: true,
-            weatherData: "",
-        }
-    }
+export default class App extends Component {
+    state = {
+        isLoading: false,
+        progress: 0,
+        progressColor: "#007aff",
+        indeterminate: true,
+        error: null,
+        temperature: null,
+        name: null,
+    };
 
     componentDidMount(){
         navigator.geolocation.getCurrentPosition(
@@ -34,33 +36,44 @@ export default class App extends React.Component {
             .then(response => response.json()) // 응답값을 json으로 변환
             .then(json => {
                 console.log(json);
-
                 this.setState({
-                    isLoading: false,
-                    weatherData: JSON.stringify(json),
-                }, function (){});
+                    isLoading: true,
+                    temperature: json.main.temp,
+                    name: json.weather[0].main,
+                })
             })
             .catch(error => {
                 console.error(error);
+                this.setState({
+                    isLoading: false,
+                    error: "Something Wrong!!",
+                    progress: 1,
+                    progressColor: "#f00",
+                })
             });
     }
 
-    render(){
-        if(this.state.isLoading){
-            return(
-                <View style={styles.container}>
-                    <Text>Now Loading....{this.state.weatherData}</Text>
-                    <Progress.Bar
-                        style={styles.progress}
-                        progress={this.state.progress}
-                        indeterminate={this.state.indeterminate}
-                    />
-                </View>
-            )
-        }
-        return(
+    render() {
+        const { isLoading, error, temperature, name } = this.state;
+        return (
             <View style={styles.container}>
-                <Text>{this.state.weatherData}</Text>
+                <StatusBar hidden={true}/>
+                {isLoading ?
+                    (
+                        <Weather weatherName={name} temp={Math.ceil(temperature - 273.15)} />
+                    ) : (
+                    <View style={styles.isLoading}>
+
+                        <Text style={styles.item}>위치 정보를 읽는 중...</Text>
+                        {error ? <Text style={styles.error}>{error}</Text> : null}
+                        <Progress.Bar
+                            style={styles.progress}
+                            progress={this.state.progress}
+                            indeterminate={this.state.indeterminate}
+                            color={this.state.progressColor}
+                        />
+                    </View>
+                )}
             </View>
         );
     }
@@ -69,6 +82,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#fff'
+    },
+    isLoading: {
+        flex:1,
         alignItems: 'center',
         justifyContent: 'center',
     },
@@ -80,6 +97,9 @@ const styles = StyleSheet.create({
     progress: {
         margin: 10,
     },
+    error:{
+        color: "red",
+    }
 });
 
 
